3c2d415335670741c3d6aa2fd63f1e5cdac8ca78
DAMIE Marc <marc.damie@inria.fr>
2025-02-26 16:35:15 +0100

    Reduce nb features to avoid memory overflow

--- sec-ml-training/experiment.py
+++ sec-ml-training/experiment.py
@@ -198,7 +198,7 @@ def varying_nb_features(laboratory):
         SGDClassifier: False,
     }
 
-    for nb_features in [4, 5, 6, 7, 8, 9, 10, 15, 20, 30, 40, 50, 100]:
+    for nb_features in [4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30]:
         laboratory.logger.info("NUMBER OF FEATURES: %d", nb_features)
         X, y = make_classification(
             n_features=nb_features,
